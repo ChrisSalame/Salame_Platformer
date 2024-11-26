51546d5b39e7b61a49e98721dd9e92d518e0582e
ChrisSalame <148483955+ChrisSalame@users.noreply.github.com>
2024-11-25 20:58:22 -0500

    task 1 completed

--- Plateforming Project/Assets/Scripts/PlayerController.cs
+++ Plateforming Project/Assets/Scripts/PlayerController.cs
@@ -6,47 +6,90 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
-    
-    public float speed;
+
+    public float accelerationTime;
+    float accelaration;
+    float velocity;
+
     Vector2 movement;
+
     //this creates a variable which the enum can set. this sets the default state as .left which 
     FacingDirection direction = FacingDirection.left;
 
+    public float apexHeight;
+    public float apexTime;
+    public float gravity;
+    float currentTime;
+    Vector2 position;
+
+
+    //I grabbed this from the codeshare
+    public float timeToReachMaxSpeed;
+    public float maxSpeed;
+    public float timeToDecelerate;
+    private float acceleration;
+    private float deceleration;
+    public float jumpPower;
 
     public enum FacingDirection
     {
         left, right
     }
 
-    
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+
+        acceleration = maxSpeed / timeToReachMaxSpeed;
+        deceleration = maxSpeed / timeToDecelerate;
+
     }
 
-   
+
     void Update()
     {
-        //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
-        //manage the actual movement of the character.
+        //this is from the codeshare
         Vector2 playerInput = new Vector2();
         MovementUpdate(playerInput);
-        GetFacingDirection();
 
-        
     }
+
 
     private void MovementUpdate(Vector2 playerInput)
     {
-        
-        //This line adds force to the rigid body, it uses the movement variable for the vector2 then adds speed 
-        rb.AddForce(movement * speed);
+        Vector2 currentVelocity = rb.velocity;
 
-        //This allows the player to input using the arrow keys
-        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        print(movement);
+        //if move the character to the left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            currentVelocity += acceleration * Vector2.left * Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            currentVelocity -= deceleration * Vector2.left * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            currentVelocity += acceleration * Vector2.right * Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            currentVelocity -= deceleration * Vector2.right * Time.deltaTime;
+        }
 
+        if (Input.GetKey(KeyCode.Space) && IsGrounded())
+        {
+            rb.gravityScale = 0;
+            currentVelocity += Vector2.up * jumpPower;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && !IsGrounded())
+        {
+            rb.gravityScale = gravity;
+        }
+
+        rb.velocity = currentVelocity;
     }
 
     public bool IsWalking()
@@ -55,7 +98,7 @@ public class PlayerController : MonoBehaviour
         {
             return true;
         }
-        else 
+        else
         {
             return false;
         }
@@ -65,16 +108,14 @@ public class PlayerController : MonoBehaviour
     public bool IsGrounded()
     {
         RaycastHit2D boxcheck;
-        boxcheck =  Physics2D.BoxCast(gameObject.transform.position, new Vector2(1, 1), 0, new Vector2(0, -1), 0.25f, LayerMask.GetMask("ground"));
+        boxcheck = Physics2D.BoxCast(gameObject.transform.position, new Vector2(1, 1), 0, new Vector2(0, -1), 0.25f, LayerMask.GetMask("ground"));
 
         if (boxcheck.collider == false)
         {
-            
             return false;
         }
         else
         {
-            
             return true;
         }
     }
